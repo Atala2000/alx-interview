415302d23cae4df5d3e20c9bd62688ffa0bbacd3
Atala2000 <atalakidi@gmail.com>
2024-03-10 20:10:55 +0300

    Changed to executable mode

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+'''A module for Pascal's triangle.
+'''
 
 
 def pascal_triangle(n):
-    """Pascal triangle function"""
-    if n <= 0:
-        return []
-    else:
-        pascal = [[1]]
-        for i in range(1, n):
-            row = [1]
-            for j in range(1, i):
-                row.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
-            row.append(1)
-            pascal.append(row)
-        return pascal
+    '''Creates the pascal triangle and returns it as
+    a list of integers
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
